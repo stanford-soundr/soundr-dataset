c9a9228f1ee3cc994ece3bb588b56a4d19c4410b
valkjsaaa <jackieyang51@gmail.com>
2019-07-01 14:01:34 -0700

    Added data pre processing for LSTM

--- preprocess.py
+++ preprocess.py
@@ -5,6 +5,7 @@ import numpy as np
 import pickle
 import scipy.signal
 import math
+import quaternion
 
 import seaborn as sns
 import matplotlib.pyplot as plt
@@ -35,13 +36,63 @@ data_sub_dirs = os.listdir(data_dir)
 input_array = []
 output_array = []
 
+segment_length = 0.1
+energy_segment_length = 0.3
+
+def get_audio_segment(audio_data, sample_rate, segment_id, override_length=None):
+    start_idx = int(sample_rate * segment_id * segment_length)
+    end_idx = int(sample_rate * (segment_id + 1) * segment_length)
+
+    if override_length is not None:
+        new_start_idx = int((start_idx + end_idx - sample_rate * override_length) / 2)
+        new_end_idx = int((start_idx + end_idx + sample_rate * override_length) / 2)
+        start_idx = new_start_idx
+        end_idx = new_end_idx
+
+    if start_idx >= 0 and end_idx < len(audio_data):
+        return audio_data[start_idx: end_idx]
+    else:
+        return None
+
+def get_tracking_sample(tracking_data, sample_rate, segment_id):
+    try:
+        idx = sample_rate * (segment_id + 0.5) * segment_length
+        prev_idx = int(math.floor(idx))
+        next_idx = int(prev_idx + 1)
+        # if tracking_data[prev_idx] is None:
+        #     prev_idx -= 1
+        #
+        # if tracking_data[next_idx] is None:
+        #     next_idx += 1
+        #
+        # prev_data = tracking_data[prev_idx]
+        # next_data = tracking_data[next_idx]
+        # if prev_data is None or next_data is None:
+        #     return None
+        #
+        # progress = (idx - prev_idx) / (next_idx - prev_idx)
+        # prev_pos = prev_data[0:3]
+        # prev_quat = quaternion.as_quat_array(prev_data[3:7])
+        # next_pos = next_data[0:3]
+        # next_quat = quaternion.as_quat_array(next_data[3:7])
+        # print(progress)
+        # idx_pos = progress * next_pos + (1 - progress) * prev_pos
+        # idx_quat = quaternion.slerp(prev_quat, next_quat, 0, 1, 0.5)
+        # return np.concatenate([idx_pos, quaternion.as_float_array(idx_quat)])
+        return tracking_data[prev_idx]
+
+    except IndexError:
+        return None
+
+
+combined_segments_data = []
+
 for data_sub_dir in data_sub_dirs:
     if data_sub_dir.startswith(".git"):
         continue
 
     if data_sub_dir.startswith("_"):
         continue
-
     data_path = os.path.join(data_dir, data_sub_dir)
 
     tracking_file_path = os.path.join(data_path, tracking_file_name)
@@ -56,50 +107,143 @@ for data_sub_dir in data_sub_dirs:
     audio_mono_data = audio_data.sum(axis=1)
     audio_energy = audio_mono_data ** 2
 
-    valid_tracking_data = []
-    valid_audio_slices = []
-    valid_audio_energy = []
+    #%%
+    audio_energy_threshold = 38
 
-    for i in range(len(tracking_data)):
-        audio_range = audio_range_from_tracking(i)
-        if not validate_audio_range(audio_range):
-            print(f"{i} is not validated!")
-        if validate_audio_range(audio_range) and tracking_data[i] is not None:
-            valid_tracking_data += [tracking_data[i]]
-            data_audio_range = audio_range_from_tracking(i, 0.16)
-            data_audio_start_index, data_audio_end_index = data_audio_range
-            audio_slice = audio_data[data_audio_start_index: data_audio_end_index]
-            valid_audio_slices += [audio_slice[0::3]]
-            audio_start_index, audio_end_index = audio_range
-            audio_energy_slice = audio_energy[audio_start_index: audio_end_index]
-            valid_audio_energy += [math.log(np.average(audio_energy_slice))]
-            # print(f"{i}: {audio_range}, {data_audio_range}")
+    def get_segments(offset = 0):
+        segments = []
+        last_valid = False
 
-    # plt.clf()
-    # valid_tracking_data_np = np.array(valid_tracking_data)
-    # sns.scatterplot(valid_tracking_data_np[:, 0], valid_tracking_data_np[:, 2], linewidth=0)
-    # plt.show()
+        for i in range(int(len(audio_mono_data) / (segment_length * audio_sample_rate))):
+            energy_segment = get_audio_segment(audio_energy, audio_sample_rate, i, override_length=0.5)
+            if energy_segment is None:
+                energy = 0
+            else:
+                energy = math.log(np.average(energy_segment))
+            audio_valid = energy > audio_energy_threshold
+            audio_segment = get_audio_segment(audio_energy, audio_sample_rate, i)
+            tracking_sample = get_tracking_sample(tracking_data, tracking_sample_rate, i)
+            valid = audio_valid and audio_segment is not None and tracking_sample is not None
+            if valid:
+                if not last_valid:
+                    segments += [[]]
+                segments[len(segments) - 1] += [(tracking_sample, audio_segment)]
+            last_valid = valid
+        return segments
 
-    speaking_tracking_data = []
-    speaking_audio_slices = []
-    speaking_audio_energy = []
+    offsets = [0, 0.0333, 0.06666]
 
-    for i in range(len(valid_tracking_data)):
-        if valid_audio_energy[i] > 39:
-            speaking_tracking_data += [valid_tracking_data[i]]
-            speaking_audio_slices += [valid_audio_slices[i]]
-            speaking_audio_energy += [valid_audio_energy[i]]
+    segments = [segment for offset in offsets for segment in get_segments(offset)]
 
-    input_array += speaking_audio_slices
-    output_array += speaking_tracking_data
+    segment_long_threshold = 20
 
-input_data = np.array(input_array)
-output_data = np.array(output_array)
+    def process_segment(segment):
+        if len(segment) > 20:
+            new_len = len(segment) / math.ceil(len(segment) / 20)
+            new_segments = []
+            for start in range(0, len(segment), int(new_len / 2)):
+                end = start + int(new_len)
+                if end > len(segment):
+                    break
+                new_segments += [segment[start:end]]
+            return new_segments
+        else:
+            return [segment]
 
-data = input_data, output_data
 
-with open("train_set2.pickle", "wb") as train_set:
-    pickle.dump(data, train_set, protocol=4)
+    segment_length_stat = [len(segment) for segment in segments]
+
+    processed_segments = [new_segment for segment in segments for new_segment in process_segment(segment)]
+
+    new_segments_length_stat = [len(segment) for segment in processed_segments]
+
+    combined_segments = []
+
+    for segment in processed_segments:
+        input_array = []
+        output_array = []
+        for data in segment:
+            input_array += [data[1]]
+            output_array += [data[0]]
+        combined_segments += [[np.array(input_array), np.array(output_array)]]
+
+    combined_segments_data += combined_segments
+
+#%%
+
+combined_segments_data_np = np.array(combined_segments_data)
+
+np.save("train_set3.npy", combined_segments_data_np)
+
+
+# #%%
+#
+#
+# data_frame_raw = []
+#
+#
+#
+# for i in range(0, len(audio_valid), 500):
+#     data_frame_raw += [['valid', i, audio_valid[i] * 1e10]]
+#     data_frame_raw += [['data', i, audio_mono_data[i]]]
+#
+# import pandas as pd
+#
+# data_frame = pd.DataFrame(data_frame_raw, columns=["type", "x", "value"])
+#
+# plt.clf()
+# plt.gcf().set_size_inches(50, 10)
+# sns.lineplot(x="x", y="value", hue="type", data=data_frame)
+# plt.savefig("/tmp/audio.png")
+# #%%
+#
+#
+#
+# valid_tracking_data = []
+# valid_audio_slices = []
+# valid_audio_energy = []
+#
+# # for i in range(len(tracking_data)):
+# for i in range(len(tracking_data)):
+#     audio_range = audio_range_from_tracking(i)
+#     if not validate_audio_range(audio_range):
+#         print(f"{i} is not validated!")
+#     if validate_audio_range(audio_range) and tracking_data[i] is not None:
+#         valid_tracking_data += [tracking_data[i]]
+#         data_audio_range = audio_range_from_tracking(i, 0.16)
+#         data_audio_start_index, data_audio_end_index = data_audio_range
+#         audio_slice = audio_data[data_audio_start_index: data_audio_end_index]
+#         valid_audio_slices += [audio_slice[0::3]]
+#         audio_start_index, audio_end_index = audio_range
+#         audio_energy_slice = audio_energy[audio_start_index: audio_end_index]
+#         valid_audio_energy += [math.log(np.average(audio_energy_slice))]
+#         # print(f"{i}: {audio_range}, {data_audio_range}")
+#
+# # plt.clf()
+# # valid_tracking_data_np = np.array(valid_tracking_data)
+# # sns.scatterplot(valid_tracking_data_np[:, 0], valid_tracking_data_np[:, 2], linewidth=0)
+# # plt.show()
+#
+# speaking_tracking_data = []
+# speaking_audio_slices = []
+# speaking_audio_energy = []
+#
+# for i in range(len(valid_tracking_data)):
+#     if valid_audio_energy[i] > 39:
+#         speaking_tracking_data += [valid_tracking_data[i]]
+#         speaking_audio_slices += [valid_audio_slices[i]]
+#         speaking_audio_energy += [valid_audio_energy[i]]
+#
+# input_array += speaking_audio_slices
+# output_array += speaking_tracking_data
+#
+# input_data = np.array(input_array)
+# output_data = np.array(output_array)
+#
+# data = input_data, output_data
+#
+# with open("train_set2.pickle", "wb") as train_set:
+#     pickle.dump(data, train_set, protocol=4)
 
 #
 #
